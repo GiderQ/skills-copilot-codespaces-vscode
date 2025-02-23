71ddfe070eb2fd89b63cca6b46323aed9cdf6444
Yaroslav Davydenko <140607922+GiderQ@users.noreply.github.com>
2025-02-23 22:27:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'Yaroslav Davydenko',
+        role: 'Game developer',
+        skills: ['C++', 'C#', 'Unity']
+    };
+
+    return member;
+}
